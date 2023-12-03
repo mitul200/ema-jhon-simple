f8cf3f0e1ee7354e6c1f77902d777b00b0520425
mitul200 <mitul5267@gmail.com>
2023-12-03 20:47:46 +0600

    added font awesome and finished the project suessfully

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product)
-    const {price,img,id,name,quantity,ratings,ratingsCount,shipping,stock,seller} = props.product
+    // console.log(props)
+    const {price,img,id,name,quantity,ratings,seller} = props.product
+    const handelClick = props.handelClick
+
+    
     return (
         <div className='product'>
           <img src={img} alt="" />
@@ -13,7 +18,10 @@ const Product = (props) => {
                 <p>Manufacturer $ : {seller}</p>
                 <p>ratings : ${ratings} star</p>
             </div>
-            <button className="btn-cart">Add to Cart</button>
+            <button onClick={()=>handelClick(props.product)} className="btn-cart">
+                Add to Cart
+                <FontAwesomeIcon icon={faShoppingCart} />
+                </button>
         </div>
     );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -4,11 +4,19 @@ import Product from '../Product/Product';
 
 const Shop = () => {
     const [products, setProducts] =useState([])
+    const [cart,setCart]=useState([])
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(res=>res.json())
         .then(data=> setProducts(data))
     },[])
+
+    const handelClick =(product)=>{
+        const newCart = [...cart , product]
+        setCart(newCart)
+
+    }
+
     return (
         <div className='shop-Container'>
             <div className="products-Container">
@@ -16,11 +24,13 @@ const Shop = () => {
                     products.map(product =><Product
                          key={product.id}
                          product={product}
+                         handelClick={handelClick}
                          ></Product>)
                 }
             </div>
             <div className="cart-Container">
-                <h5>product summary</h5>
+                <h2>Order Summary </h2>
+                <h4>product quentity : {cart.length}</h4>
             </div>
         </div>
     );
