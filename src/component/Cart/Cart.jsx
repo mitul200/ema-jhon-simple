747828b74e7a5aab359738607174e22f24852809
mitul200 <mitul5267@gmail.com>
2023-12-04 13:40:22 +0600

    added griend price

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -5,22 +5,28 @@ const Cart = ({cart}) => {
     // const {cart} = props //steps 1//
     // console.log(cart)
 
+    // total added ----
     let total = 0;
+    let totalShipping = 0
     for (const product of cart) {
         // console.log(product)
          total = total + product.price
-        console.log(total)
-        
+         totalShipping = totalShipping + product.shipping
+        console.log(totalShipping)  
     }
+    // tax total -----
+    const tax = total*7/100
+
+    const grandTotal = tax + total + totalShipping
+
     return (
         <div className='cart'>
             <h2>Order Summary </h2>
                 <h4>product quentity : {cart.length}</h4>
-                <p>Selected Items: 6</p>
                 <p>Total Price: ${total}</p>
-                <p>Total Shipping Charge: $5</p>
-                <p>Tax: $114</p>
-                <h5>Grand Total: $1559</h5>
+                <p>Total Shipping Charge: {totalShipping}</p>
+                <p>Tax: ${tax.toFixed(2)}</p>
+                <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
         </div>
     );
 };
